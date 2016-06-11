7f00ac3bba5750c73a666afdab27a6ddd44bf359
Sai Deep <saideepbsd@gmail.com>
2016-06-11 13:27:54 +0530

    Q3) Print fibonacci series upto 1000 using 'yield'.
    Done.

---
+++ fibonacci.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+
+# Fibonacci series upto 1000 using 'yield'.
+def fibonacci(n)   
+    
+    a, b = 1, 1 # parallel assignment  
+    
+    while a <= n 
+        yield a 
+        a, b = b, a + b 
+    end
+
+end
+
+fibonacci(1000) {|i| puts i} # Print fibonacci series upto 1000. 
