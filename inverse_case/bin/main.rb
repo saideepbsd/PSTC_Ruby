6d2b8164b037da4d4b1168e5e32ca3537ce69a3e
Sai Deep <saideepbsd@gmail.com>
2016-07-05 23:43:12 +0530

    Add problem of inversing case of a string Overwrite the default 'to_s' method such that it inverses the case of each letter.

---
+++ inverse_case/bin/main.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/string.rb'
+
+puts 'Enter any string'
+input = gets.chomp
+puts input.to_s
+
---
+++ inverse_case/lib/string.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+class String
+	# Overwrites the default 'to_s' method such that it inverses the case of each letter.
+	def to_s
+		swapcase
+	end
+
+end	
+
